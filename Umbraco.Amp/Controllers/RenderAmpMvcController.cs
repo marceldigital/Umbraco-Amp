a6f8bc98f9a2e17469fda5b920e153855e4383c5
Alex Vilmur <alex@vilmurnet.com>
2017-05-05 15:38:57 -0500

    Imported files from private solution.

---
+++ Umbraco.Amp/Controllers/RenderAmpMvcController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+using Umbraco.Web.Models;
+using Umbraco.Web.Mvc;
+
+namespace MarcelDigital.Umbraco.Amp.Controllers
+{
+    public class RenderAmpMvcController : RenderMvcController {
+        /// <summary>
+        /// Hijacks the Amp alternate template route and routes it to
+        /// the AMO view, returns 404 if no view is found.
+        /// </summary>
+        public virtual ActionResult Amp(RenderModel model) {
+            return AmpTemplate(model);
+        }
+
+        /// <summary>
+        /// Finds the AMP view for the document type in the /Views/Amp
+        /// folder.
+        /// </summary>
+        public ActionResult AmpTemplate(RenderModel model) {
+            var template = $"/Views/Amp/{model.Content.DocumentTypeAlias}.cshtml";
+            // If the template does not exist go to a 404 page because there is no AMP version
+            if (EnsurePhsyicalViewExists(template) == false)
+                return new HttpNotFoundResult("No AMP version of this content found.");
+            return View(template, model);
+        }
+    }
+}
---
+++ Umbraco.Amp/Specifications/Templates/AmpSpecification.cs
@@ -0,0 +1,19 @@
+﻿using MarcelDigital.UmbracoExtensions.StarterKit.Core.Specifications;
+
+namespace MarcelDigital.Umbraco.Amp.Specifications.Templates {
+    class AmpSpecification : ITemplateSpecification {
+        public string Name => "AMP";
+
+        public string Alias => "Amp";
+
+        public string Content => @"
+@inherits UmbracoTemplatePage
+@*
+    Redirects back to the canonical version
+*@
+@{
+    Response.Redirect(Model.Content.Url);
+}
+";
+    }
+}
